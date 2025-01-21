3278d04ee864e8d30675ff6043334f47a2f86dbe
Gabriel Bursi <gabriellbursii@gmail.com>
2025-01-20 22:08:53 -0300

    first commit

---
+++ src/app/app.ts
@@ -0,0 +1,15 @@
+import cors from 'cors'
+import express from 'express'
+const app = express()
+app.disable('x-powered-by')
+
+app.use(cors({ origin: 'http://localhost:3333' }))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (_req, res) => {
+	res.send('Sever is healthy')
+})
+
+export default app
